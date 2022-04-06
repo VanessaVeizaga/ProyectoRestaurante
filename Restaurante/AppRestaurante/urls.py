89fddae8daeda016fbde3c1e26d4457fdc48533b
VanessaVeizaga <vanessaveizaga@gmail.com>
2022-04-06 17:44:05 -0300

    Creación de vistas y urls

---
+++ Restaurante/AppRestaurante/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from AppRestaurante import views
+
+urlpatterns = [
+    path('""/', views.inicio, name="Inicio"),
+    path('menu/', views.menu, name="Menu"),
+    path('local/', views.local, name="Local"),
+    path('reserva/', views.reserva, name="Reserva"),
+    path('contacto/', views.contacto, name="Contacto"),
+]
--- Restaurante/AppRestaurante/views.py
+++ Restaurante/AppRestaurante/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def inicio(request):
+    return HttpResponse("Vista Inicio")
+
+def menu(request):
+    return HttpResponse("Vista Menú")
+
+def local(request):
+    return HttpResponse("Vista Local")
+
+def reserva(request):
+    return HttpResponse("Vista Reserva") 
+
+def contacto(request):
+    return HttpResponse("Vista Contacto")        
+
+
